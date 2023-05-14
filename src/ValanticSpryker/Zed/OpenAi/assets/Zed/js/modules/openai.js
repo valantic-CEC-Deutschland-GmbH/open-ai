849662e2d2ca1f4f0dbd0944e39a89ac3eb1acf0
Patrick Schönfeld <p.schoenfeld@nexus-united.com>
2023-05-14 03:47:20 +0200

    no-task escape ai response

--- src/ValanticSpryker/Zed/OpenAi/assets/Zed/js/modules/openai.js
+++ src/ValanticSpryker/Zed/OpenAi/assets/Zed/js/modules/openai.js
@@ -117,7 +117,7 @@ $(document).ready(function () {
             modalJQuery.modal("show");
             loading("Executing Prompt...");
             executePrompt(eventName, context, function(data) {
-                selectBox.innerHTML = data.choices.map(completionChoice => `<option title='${completionChoice.text}'`+ "value='"+escapeHtml(completionChoice)+"'>"+completionChoice.text.substring(0,70)+"</option>").join('');
+                selectBox.innerHTML = data.choices.map(completionChoice => `<option title='${completionChoice.text}'`+ "value='"+escapeHtml(completionChoice.text)+"'>"+completionChoice.text.substring(0,70)+"</option>").join('');
                 loaded();
                 modalJQuery.find(".modal-footer button").unbind("click");
                 modalJQuery.find(".modal-footer button").on("click", function(event) {
